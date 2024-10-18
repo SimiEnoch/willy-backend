664bdbcd28f91dcc447e58623e697454b1e9a853
Simi Enoch <similoluwaenoch@gmail.com>
2024-10-18 12:03:04 +0100

    My  Commit

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
+const MONGODBURI = process.env.MONGODBURI || 'mongodb://localhost/willy'
+
 module.exports = function () {
   mongoose
-    .connect('mongodb://localhost/willy')
+    .connect(MONGODBURI)
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.log('Could not connect to MongoDB'));
 };
